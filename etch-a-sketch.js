ee9bf4f1cc0f44de481023092b7532e5f2cda250
Mike <mikemania32@hotmail.com>
2021-11-23 12:27:53 -0700

    version that works, even after refresh

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,29 +1,29 @@
-//ROCK/PAPER/SCISSORS GAME
-//first to 5 successful rounds of play wins
+//etch-a-sketch
+//builds an nXn grid of squares that change color when mouse hovers over them
+//user can build and specify size of new grid by pressing the REFRESH button
 
 let squaresPerSide = 4;
 let squareArray = [];
 let containerArray = [];
 let mainDiv = document.getElementById("mainDiv");
-
-console.log(mainDiv)
-
+let refreshButton = document.getElementById('refreshButton');
+refreshButton.addEventListener('click', (e) => {
+    refreshGrid();
+});
 
 buildGrid();
+listenForSquareClicks();
 
+function listenForSquareClicks() {
 let squareElements = document.querySelectorAll(".square");
 squareElements.forEach((square) => 
     square.addEventListener('mouseover', (e) => {
         square.style.backgroundColor = 'black';
 }));
-    
-let resetButton = document.getElementById('resetButton');
-resetButton.addEventListener('click', (e) => {
-    getGridSize();
-});
+}
 
 function buildGrid() {
-//create an array of container elements
+    //create an array of container elements, which will contain rows of squares
     for (i = 0; i < (squaresPerSide); i += 1) {
         containerArray[i] = document.createElement('div');
         containerArray[i].setAttribute('style', 'display: flex; jusifyContent: center; alignItems: center');
@@ -48,72 +48,20 @@ function buildGrid() {
     }
 }
 
-function getGridSize() {
+function refreshGrid() {
     deleteGrid();
     squaresPerSide = window.prompt("Please Enter Number of Squares per Side");
     buildGrid();
+    listenForSquareClicks();
 }
 
 function deleteGrid() {
     for (i = 0; i < (squaresPerSide); i += 1) {
         mainDiv.removeChild(containerArray[i]);
     }
-    squareArray = [];
-    containerArray = [];
+
 }
 
 
 
 
-
-
-
-
-
-
-
-/*
-create a 16x16 grid of div elements
-
-create grid
--for each row:
- -create container div
- -create square div until # of squares per side is reached
-when mouse hovers over div elements
--change div element color to black
-
-reset button resets all div colors and prompt user for # of squares per side in next grid
-
-
-*/
-
-/*this is what the 16x16 should look like
-<div>
-<div class = container>
-  <div class = square></div>
-  <div class = square></div>
-  <div class = square></div>
-  <div class = square></div>
-</div>
-<div>
-<div class = container>
-  <div class = square></div>
-  <div class = square></div>
-  <div class = square></div>
-  <div class = square></div>
-</div>
-<div>
-<div class = container>
-  <div class = square></div>
-  <div class = square></div>
-  <div class = square></div>
-  <div class = square></div>
-</div>
-<div>
-<div class = container>
-  <div class = square></div>
-  <div class = square></div>
-  <div class = square></div>
-  <div class = square></div>
-</div>
-*/
